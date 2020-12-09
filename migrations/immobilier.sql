55a07781ac5498bc63a791f6cd76bf9077fced11
alexis <skodak95alexis@gmail.com>
2020-12-09 16:57:33 +0100

    Updated SQL dump

--- migrations/immobilier.sql
+++ migrations/immobilier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 09, 2020 at 03:19 PM
+-- Generation Time: Dec 09, 2020 at 04:56 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -50,7 +50,7 @@ INSERT INTO `logement` (`id`, `titre`, `adresse`, `ville`, `cp`, `surface`, `pri
 (3, 'Studio 1 pièce 20 m²', '5 Grande Rue de la Guillotiere', 'Lyon', 69007, 20, 565, 'studio1.jpg', 'location', 'Studio meublé et équipé - Métro Jean Jaurès / quartier Gerland\r\n\r\nOffre Flash : frais de dossier offerts pour tout début de bail avant le 31 décembre 2020 !!\r\n\r\nLa résidence étudiante LE UP se situe au coeur du quartier de Gerland, riche de grands espaces verts, de sites majeurs et de bâtiments historiques aux architectures remarquables (Stade de Gerland, Halle Tony Garnier...). Les nombreux commerces et infrastructures de loisirs aux alentours, les transports en commun (métro et tramway à quelques ...'),
 (6, 'Studio 20m2 Gorge de loup', '553 Avenue du Marechal Leclerc', 'Lyon', 69009, 20, 550, 'studio2.jpg', 'location', 'Studio meublé 20m2  Métro Gorge de Loup\r\n2ème étage avec ascenseur + interphone\r\nEntrée, placard, cuisine équipée, salle de bain, chauffage individuel\r\nLibre de suite et direct propriétaire\r\nLoyer 550€ charges comprises'),
 (7, 'T3 avec terrasse', '272 rue Paul Bert', 'Lyon', 69003, 58, 973, 'apart3.jpg', 'location', 'Au 272, rue Paul Bert - Proximité bus et commerces - Dans un immeuble de 2016 - Au 5ème étage avec ascenseur, beau T3, de 58.70 m², comprenant une entrée, un séjour avec cuisine ouverte aménagée et semi-équipée (meubles + hotte), deux chambres, une salle de bain et un WC. Terrasse de 40.85 m² et balcon de 3.50 m². Chauffage individuel à gaz. Garage au sous-sol. Libre le 21/12/2020.\r\n\r\nLoyer hors charges: 883.68 euros - Provision pour charges: 90.00 euros (avec régularisation annuelle)- Soit un loyer charges comprises de: 973.68 euros - Dépôt de garantie: 883.00 euros - Honoraires de location à la charge du locataire TTC: 763.10 euros (soit 13 euros / m²) dont état des lieux d\'entrée TTC: 176.10 euros ( soit 3 euros / m²).'),
-(8, 'Appartement T2 Quai de Saone', '99 Quai de la Saone', 'Lyon', 69009, 49, 830, 'apart4.jpg', '', 'Particulier loue T2 Grande Rue de Saint Rambert dans propriété de caractère, 3ème étage, belle vue sur terrasse-jardin et la Saône, face à l\'Ile Barbe. \r\n\r\nAppartement calme et lumineux avec des prestations de qualité :\r\nIsolation soignée, \r\nFenêtres double vitrage, \r\nSéjour avec cuisine aménagée (plaque vitro céramique, combiné réfrigérateur / congélateur, four à micro ondes...),\r\nChambre, \r\nSdb avec douche à l\'italienne, meuble vasque et WC,\r\nChauffage électrique avec radiateurs fonte ductile Noirot, \r\nInterphone, \r\nFibre optique, \r\nParking vélo. \r\n\r\nBus 31, 40 et 43 station métro Vaise à 10 minutes; Vélo\'v; \r\nCharme des bords de Saône, \r\nEsprit village au pied des monts d\'Or, \r\nTous commerces de proximité.\r\n\r\nDisponible de suite. \r\n\r\nLoyer : 830 € charges comprises\r\nDépôt de garantie : 815 €\r\nMontant des charges : 15€ / mois \r\nPas de frais d\'agence, ni d\'honoraires.  \r\nGaranties financières demandées. ');
+(11, 'Joli t2 15e arr.', '123 Rue petite', 'Paris', 75015, 48, 999, 'IMG-5fd0f0750248f0.64328288.jpg', 'location', 'Appartement calme et lumineux avec des prestations de qualité :\r\nIsolation soignée, \r\nFenêtres double vitrage, \r\nSéjour avec cuisine aménagée (plaque vitro céramique, combiné réfrigérateur / congélateur, four à micro ondes...),\r\nChambre, \r\nSdb avec douche à l\'italienne, meuble vasque et WC,\r\nChauffage électrique avec radiateurs fonte ductile Noirot, \r\nInterphone, \r\nFibre optique, \r\nParking vélo. ');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +70,7 @@ ALTER TABLE `logement`
 -- AUTO_INCREMENT for table `logement`
 --
 ALTER TABLE `logement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
